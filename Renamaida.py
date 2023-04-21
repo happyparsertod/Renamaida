a00b74d25c9315f750fb9dae8f3b0ffde6fc2a38
parsertod <serdarovihlas@yahoo.com>
2023-04-21 17:55:49 +0300

    Update Renamaida.py
    JSON generation and renaming now combined into one file

--- Renamaida.py
+++ Renamaida.py
@@ -6,140 +6,213 @@ import json
 import idc
 import os
 
+arm_dict = {
+    "arm": {
+        "MOV": "a",
+        "MVN": "b",
+        "ADD": "c",
+        "SUB": "d",
+        "MUL": "e",
+        "LSL": "f",
+        "LSR": "g",
+        "ASR": "h",
+        "ROR": "i",
+        "CMP": "j",
+        "AND": "k",
+        "ORR": "l",
+        "EOR": "m",
+        "LDR": "n",
+        "STR": "o",
+        "LDM": "p",
+        "STM": "q",
+        "PUSH": "r",
+        "POP": "s",
+        "B": "t",
+        "BL": "u",
+        "BLX": "v",
+        "BEQ": "w",
+        "SWI": "x",
+        "SVC": "y",
+        "NOP": "z",
+    },
+    "metapc": {
+        "MOV": "A",
+        "ADD": "B",
+        "SUB": "C",
+        "CMP": "D",
+        "JMP": "E",
+        "JE": "F",
+        "JNE": "G",
+        "JZ": "H",
+        "JNZ": "I",
+        "CALL": "J",
+        "RET": "K",
+        "PUSH": "L",
+        "POP": "M",
+        "LEA": "N",
+        "AND": "O",
+        "OR": "P",
+        "XOR": "Q",
+        "NOT": "R",
+        "SHL": "S",
+        "SHR": "T",
+        "ROL": "U",
+        "ROR": "V",
+        "TEST": "W",
+        "LOOP": "X",
+        "INT": "Y",
+        "NOP": "Z",
+    },
+    "ppc": {
+        "add": "a",
+        "addc": "b",
+        "addi": "c",
+        "addic": "d",
+        "and": "e",
+        "andi.": "f",
+        "b": "g",
+        "bc": "h",
+        "bca": "i",
+        "bcl": "j",
+        "bclr": "k",
+        "cmp": "l",
+        "cmpl": "m",
+        "cntlzw": "n",
+        "crand": "o",
+        "cror": "p",
+        "crxor": "q",
+        "lbz": "r",
+        "li": "s",
+        "lis": "t",
+        "lwz": "u",
+        "mflr": "v",
+        "mtlr": "w",
+        "mullw": "x",
+        "or": "y",
+        "ori": "z",
+        "rlwinm": "A",
+        "srw": "B",
+        "stw": "C",
+        "subf": "D",
+        "subi": "E",
+        "xor": "F",
+    },
+    "mips": {
+        "add": "A",
+        "addi": "B",
+        "addiu": "C",
+        "addu": "D",
+        "and": "E",
+        "andi": "F",
+        "beq": "G",
+        "bne": "H",
+        "j": "I",
+        "jal": "J",
+        "jr": "K",
+        "lbu": "L",
+        "lhu": "M",
+        "ll": "N",
+        "lui": "O",
+        "lw": "P",
+        "nor": "Q",
+        "or": "R",
+        "ori": "S",
+        "sb": "T",
+        "sh": "U",
+        "slti": "V",
+        "sltiu": "W",
+        "sll": "X",
+        "srl": "Y",
+        "sw": "Z",
+    },
+}
 
-class Renamaida(idaapi.action_handler_t):
-    def __init__(self, signature_base_name):
+# =========================================================================================#
+
+
+class RenamaidaPlugin(idaapi.plugin_t):
+    flags = 0
+    rename_action_name = "renamaida:rename"
+    produce_action_name = "renamaida:produce"
+
+    rename_menu_path = "Edit/Renamaida/Rename functions"
+    produce_menu_path = "Edit/Renamaida/Generate JSON"
+
+    comment = "Plugin for renaming unknown functions and generating JSON signature"
+    help = ""
+    wanted_name = "Renamaida"
+    wanted_hotkey = ""
+
+    def init(self):
+        # Register the action
+        rename_unk_funcs = idaapi.action_desc_t(
+            self.rename_action_name,
+            "Rename functions",
+            RenamaidaRename(),
+            None,
+            None,
+            199,
+        )
+
+        produce_json_sign = idaapi.action_desc_t(
+            self.produce_action_name,
+            "Generate JSON",
+            RenamaidaProduce(),
+            None,
+            None,
+            199,
+        )
+
+        idaapi.register_action(rename_unk_funcs)
+        idaapi.register_action(produce_json_sign)
+
+        idaapi.attach_action_to_menu(
+            self.rename_menu_path, self.rename_action_name, idaapi.SETMENU_APP
+        )
+        idaapi.attach_action_to_menu(
+            self.produce_menu_path, self.produce_action_name, idaapi.SETMENU_APP
+        )
+
+        return idaapi.PLUGIN_KEEP
+
+    def run(self, arg):
+        pass
+
+    def term(self):
+        idaapi.detach_action_from_menu(self.rename_menu_path, self.rename_action_name)
+        idaapi.detach_action_from_menu(self.produce_menu_path, self.produce_action_name)
+        return
+
+
+# =========================================================================================#
+
+
+class RenamaidaRename(idaapi.action_handler_t):
+    def __init__(self):
         idaapi.action_handler_t.__init__(self)
 
         self.arch = idaapi.get_inf_structure().procName.lower()
-        self.signature_base_name = signature_base_name
-
-        self.arch_instructions_dict = {
-            "arm": {
-                "MOV": "a",
-                "MVN": "b",
-                "ADD": "c",
-                "SUB": "d",
-                "MUL": "e",
-                "LSL": "f",
-                "LSR": "g",
-                "ASR": "h",
-                "ROR": "i",
-                "CMP": "j",
-                "AND": "k",
-                "ORR": "l",
-                "EOR": "m",
-                "LDR": "n",
-                "STR": "o",
-                "LDM": "p",
-                "STM": "q",
-                "PUSH": "r",
-                "POP": "s",
-                "B": "t",
-                "BL": "u",
-                "BLX": "v",
-                "BEQ": "w",
-                "SWI": "x",
-                "SVC": "y",
-                "NOP": "z",
-            },
-            "metapc": {
-                "MOV": "A",
-                "ADD": "B",
-                "SUB": "C",
-                "CMP": "D",
-                "JMP": "E",
-                "JE": "F",
-                "JNE": "G",
-                "JZ": "H",
-                "JNZ": "I",
-                "CALL": "J",
-                "RET": "K",
-                "PUSH": "L",
-                "POP": "M",
-                "LEA": "N",
-                "AND": "O",
-                "OR": "P",
-                "XOR": "Q",
-                "NOT": "R",
-                "SHL": "S",
-                "SHR": "T",
-                "ROL": "U",
-                "ROR": "V",
-                "TEST": "W",
-                "LOOP": "X",
-                "INT": "Y",
-                "NOP": "Z",
-            },
-            "ppc": {
-                "add": "a",
-                "addc": "b",
-                "addi": "c",
-                "addic": "d",
-                "and": "e",
-                "andi.": "f",
-                "b": "g",
-                "bc": "h",
-                "bca": "i",
-                "bcl": "j",
-                "bclr": "k",
-                "cmp": "l",
-                "cmpl": "m",
-                "cntlzw": "n",
-                "crand": "o",
-                "cror": "p",
-                "crxor": "q",
-                "lbz": "r",
-                "li": "s",
-                "lis": "t",
-                "lwz": "u",
-                "mflr": "v",
-                "mtlr": "w",
-                "mullw": "x",
-                "or": "y",
-                "ori": "z",
-                "rlwinm": "A",
-                "srw": "B",
-                "stw": "C",
-                "subf": "D",
-                "subi": "E",
-                "xor": "F",
-            },
-            "mips": {
-                "add": "A",
-                "addi": "B",
-                "addiu": "C",
-                "addu": "D",
-                "and": "E",
-                "andi": "F",
-                "beq": "G",
-                "bne": "H",
-                "j": "I",
-                "jal": "J",
-                "jr": "K",
-                "lbu": "L",
-                "lhu": "M",
-                "ll": "N",
-                "lui": "O",
-                "lw": "P",
-                "nor": "Q",
-                "or": "R",
-                "ori": "S",
-                "sb": "T",
-                "sh": "U",
-                "slti": "V",
-                "sltiu": "W",
-                "sll": "X",
-                "srl": "Y",
-                "sw": "Z",
-            },
-        }
 
         self.unknown_func_names = {}
         self.comparison_result = {}
 
-        with open(self.signature_base_name) as f:
-            self.signature_base_json = json.load(f)
+    def load_signature_file(self):
+        # Prompt the user to select a file from a directory
+        file_path = idaapi.ask_file(0, "*.json", "Select signature base file")
+        if not file_path:
+            return None
+
+        # Get the file name from the path
+        return os.path.abspath(file_path)
+
+    def activate(self, ctx):
+        self.signature_base_name = self.load_signature_file()
+
+        if self.signature_base_name != None:
+            with open(self.signature_base_name) as f:
+                self.signature_base_json = json.load(f)
+        else:
+            return
 
         self.make_unknown_base()
         self.make_comparison()
@@ -154,8 +227,8 @@ class Renamaida(idaapi.action_handler_t):
             if function_name.startswith("__") or not function_name.startswith("sub"):
                 continue
 
-            if self.arch in self.arch_instructions_dict:
-                architecture_instructions = self.arch_instructions_dict[self.arch]
+            if self.arch in arm_dict:
+                architecture_instructions = arm_dict[self.arch]
                 function_instructions = "".join(
                     [
                         architecture_instructions[idc.print_insn_mnem(ea)]
@@ -243,44 +316,57 @@ class Renamaida(idaapi.action_handler_t):
 
         print(f"Renamaida: {len(self.comparison_result)} functions renamed!")
 
+    def update(self, ctx):
+        return idaapi.AST_ENABLE_ALWAYS
 
-class RenamaidaPlugin(ida_idaapi.plugin_t):
-    flags = ida_idaapi.PLUGIN_FIX | ida_idaapi.PLUGIN_PROC
 
-    explain_action_name = "Renamaida::rename_functions"
-    explain_menu_path = "Edit/Renamaida"
-    comment = "Description of the Renamaida plugin"
-    help = "Usage: RenamaidaPlugin"
-    wanted_name = "Renamaida"
-    wanted_hotkey = ""
+# =========================================================================================#
 
-    def init(self):
-        return idaapi.PLUGIN_KEEP
 
-    def run(self, arg):
-        # Prompt the user to select a file from a directory
-        file_path = idaapi.ask_file(0, "*.json", "Select signature base file")
-        if not file_path:
+class RenamaidaProduce(idaapi.action_handler_t):
+    def __init__(self):
+        idaapi.action_handler_t.__init__(self)
+
+        self.function_dict = {}
+        self.arch = idaapi.get_inf_structure().procName.lower()
+
+    def activate(self, ctx):
+        for function_ea in idautils.Functions():
+            function_name = idc.get_func_name(function_ea)
+
+            if function_name.startswith("__"):
+                continue
+
+            if self.arch in arm_dict:
+                architecture_instructions = arm_dict[self.arch]
+                function_instructions = "".join(
+                    [
+                        architecture_instructions[idc.print_insn_mnem(ea)]
+                        for ea in idautils.FuncItems(function_ea)
+                        if idc.print_insn_mnem(ea) in architecture_instructions
+                    ]
+                )
+
+            if len(function_instructions) < 10:
+                continue
+
+            self.function_dict[function_name] = function_instructions
+
+        fname = idaapi.ask_file(1, "*.json", "Save signature file as")
+
+        if fname:
+            with open(fname, "w") as f:
+                json.dump(self.function_dict, f)
+        else:
             return
 
-        # Get the file name from the path
-        signature_base_name = os.path.abspath(file_path)
+        print("Renamaida: Signature JSON file saved!")
 
-        # Register the action
-        rename_unk_funcs = idaapi.action_desc_t(
-            self.explain_action_name,
-            "Rename all functions",
-            Renamaida(signature_base_name),
-            "Ctrl+Alt+M",
-            "Rename all unknown functions imported from public libraries",
-            200,
-        )
-        idaapi.register_action(rename_unk_funcs)
+    def update(self, ctx):
+        return idaapi.AST_ENABLE_ALWAYS
 
-        return idaapi.PLUGIN_KEEP
 
-    def term(self):
-        idaapi.detach_action_from_menu(self.explain_menu_path, self.explain_action_name)
+# =========================================================================================#
 
 
 def PLUGIN_ENTRY():
